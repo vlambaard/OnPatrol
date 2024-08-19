e8a01163990e5dd8519b394b33ec5c2e4e80f8c5
vlambaard <mw.van.vuuren@gmail.com>
2024-08-19 22:00:16 +0200

    Changed for my own environment

--- create_venv.sh
+++ create_venv.sh
@@ -4,15 +4,15 @@ sudo apt update
 sudo apt install -y screen python3-pip python3-venv python3-h5py tk-dev libncurses5-dev libncursesw5-dev libreadline6-dev libdb5.3-dev libgdbm-dev libsqlite3-dev libssl-dev libbz2-dev libexpat1-dev liblzma-dev zlib1g-dev libffi-dev cmake build-essential libatlas-base-dev 
 
 echo "Creating Python VENV"
-python -m venv ./VENV
+python3 -m venv .venv
 echo "Switching to Python VENV"
-source ./VENV/bin/activate
+source .venv/bin/activate
 echo "Installing dependencies..."
-python -m pip install --no-input -U pip
-python -m pip install --no-input -U setuptools
-python -m pip install --no-input -U wheel
-python -m pip install --no-input -r ./dependencies/requirements.txt
+python3 -m pip install --no-input -U pip
+python3 -m pip install --no-input -U setuptools
+python3 -m pip install --no-input -U wheel
+python3 -m pip install --no-input -r dependencies/requirements.txt
 
 echo "Installing custom dependencies..."
-cd ./dependencies/sqlite3worker/
-python setup.py install
+cd dependencies/sqlite3worker/
+python3 setup.py install
